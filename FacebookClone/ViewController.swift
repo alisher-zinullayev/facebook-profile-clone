b6da318ac1b6f6f78cb02850beed44829ced1a2a
Alisher <zinullayev09@gmail.com>
2023-07-08 22:21:25 +0600

    Initial Commit

---
+++ FacebookClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FacebookClone
+//
+//  Created by Alisher Zinullayev on 08.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
